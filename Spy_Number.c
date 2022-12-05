7ab0e0b68230ce42cbf4c0d26a9f828beb65d153
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2022-12-05 21:55:17 +0530

    #include<stdio.h> int main() {     int rem,sum=0,mul=1,n;     scanf("%d",&n);     while(n>0)     {         rem=n%10;         sum=sum+rem;         mul=mul*rem;         n=n/10;
        }
          if(sum==mul)
          {
              printf("Spy Number");
          }
          else
          {
              printf("Not Spy Number");
          }
        
    }

---
+++ Spy_Number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int rem,sum=0,mul=1,n;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        rem=n%10;
+        sum=sum+rem;
+        mul=mul*rem;
+        n=n/10;
+        
+    }
+      if(sum==mul)
+      {
+          printf("Spy Number");
+      }
+      else
+      {
+          printf("Not Spy Number");
+      }
+    
+}
