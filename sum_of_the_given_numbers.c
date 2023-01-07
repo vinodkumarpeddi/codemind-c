6301c0ddab52870d8a4675a5ca1120796fd48e5c
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2023-01-07 18:17:14 +0530

    #include<stdio.h> int main() {     int i,n;     scanf("%d",&n);     int a[n],b[n];     for(i=0;i<n;i++)     {         scanf("%d%d",&a[i],&b[i]);     }     for(i=0;i<n;i++)     {         printf("%d ",a[i]+b[i]);     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    int a[n],b[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d%d",&a[i],&b[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d
+",a[i]+b[i]);
+    }
+}
